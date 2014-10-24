10b0a4d46fdf2b7c7c5b721878dfc2f6fc12c0b7
pipo <psychedelic.nekopunch@gmail.com>
2014-10-24 23:57:41 +0900

    small fix

--- how_condition_does_a_value_in_service_factory_keep/controllers/ChildController.js
+++ how_condition_does_a_value_in_service_factory_keep/controllers/ChildController.js
@@ -4,24 +4,25 @@ app.controller('ChildController', function ($scope, FactorySample)
 {
 	var self = this;
 
-	self.inputValue = '';
+	self.inputValue  = '';
 	self.inputValue2 = '';
-	self.getValue = null;
-	self.getValue2 = null;
+	self.getValue    = null;
+	self.getValue2   = null;
 
 	self.setVal = function()
 	{
-		if(!FactorySample.setVal(self.inputValue)){
+		if (!FactorySample.setVal(self.inputValue)) {
 			console.log('same param : 1');
 		}
-		if(!FactorySample.setVal2(self.inputValue2)){
+
+		if (!FactorySample.setVal2(self.inputValue2)) {
 			console.log('same param : 2');
 		}
 
 		self.getValue = FactorySample.getVal();
 	};
 
-	$scope.$watch('factorySampleVal', function (data)
+	$scope.$watch('factorySampleVal', function(data)
 	{
 		self.getValue2 = data;
 	});
