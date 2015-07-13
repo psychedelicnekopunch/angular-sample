16f4a7a5f79d87628a8646d5c725d8cd3f0e6e2b
pipo <psychedelic.nekopunch@gmail.com>
2015-07-14 05:00:45 +0900

    small fix

--- scroll_event_tracking/controllers/IndexController.js
+++ scroll_event_tracking/controllers/IndexController.js
@@ -2,18 +2,10 @@ app.controller('IndexController', function($scope)
 {
 	'use strict';
 
-	var self = this;
-
-	self.init = function()
-	{
-
-	};
-
 	// you can get scroll event tracking here
 	$scope.$root.$on('scrolleventtracking', function(e, res)
 	{
 		console.log(res);
 	});
 
-	self.init();
 });
